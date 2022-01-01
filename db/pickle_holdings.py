7c553cd6eae1321439907faaaba45b3767224588
Declan Gray-Mullen <dgraymullen@umass.edu>
2022-01-01 18:04:10 -0500

    complete xlsx refactor
    color selection refactor, code refactor/simplification in both frontend/backend

---
+++ db/pickle_holdings.py
@@ -0,0 +1,66 @@
+# generating the sectors automatically takes a long time (~1 sec per equity. thanks yahoo -_- )
+# run this separate script to add sector column when we want to change chart content (holdings.xlsx -> holdings.pkl)
+import pandas as pd
+import yfinance as yf
+
+
+def merge_row(df, idx_to_add_to, row_to_add, cols_to_merge):
+    for col_name in cols_to_merge:
+        val_add = row_to_add[col_name]
+        orig = df.at[idx_to_add_to, col_name]
+        if type(val_add) is not None and type(orig) is not None:
+            df.at[idx_to_add_to, col_name] = (val_add + orig)
+
+
+# process xlsx into df
+def get_df(filepath="holdings.xlsx"):
+    # primary key of df
+    prim_key = "Symbol"
+    # keep columns
+    keeps = [prim_key, "Product Type", "Name", "Market Value ($)", "Quantity", "Last ($)", "Adjusted Cost ($)"]
+    # reads from filepath in cwd, column labels included, force primary key
+    df = pd.read_excel(filepath, header=1, na_values="-", usecols=keeps).drop_duplicates(prim_key)
+    # merge pending transaction costs in if they exist
+    clearing = df[df["Symbol"] == "CLER"]
+    df.drop(clearing.index, inplace=True)
+    col_to_merge = ["Market Value ($)"]
+    cash_idx = df[df["Symbol"] == "BDPS"].index[0]
+    merge_row(df, cash_idx, clearing, col_to_merge)
+
+    return df
+
+# use yfinance to auto grab sector labels
+def add_sector_labels(df, label):
+    df.sort_values(by=["Product Type"], inplace=True, ascending=False)  # any non-equity belongs to Cash sector
+    sectors = []
+
+    # EQUITIES
+    is_equity = df["Product Type"].eq("Stocks / Options")
+    ticker_list = list(df["Symbol"][is_equity])
+    infos = yf.Tickers(ticker_list)
+    for ticker in infos.tickers.keys():
+        print("Determining sector for:", ticker)
+        sector = str(infos.tickers[ticker].info['sector'])
+        print(ticker, "is", sector)
+        sectors.append(sector)
+
+    print(sectors)
+    sectors = condense_sector_aliases(sectors)
+    print(sectors)
+
+    # CASH - all non-equity holdings
+    is_cash = [not e for e in is_equity]
+
+    df[label] = sectors + ["Cash" for i in range(sum(is_cash))]
+
+    return df
+
+def condense_sector_aliases(sectors):
+    return list(map(lambda a: 'Technology' if (a == 'Communication Services') else a, sectors))
+
+if __name__=='__main__':
+    df = get_df()
+    df = add_sector_labels(df, "Sector")
+    # TODO - condense sector aliases
+    df.to_pickle("holdings")
+
---
+++ src/parseXLSX.py
@@ -0,0 +1,71 @@
+import datetime
+import pandas as pd
+from collections import defaultdict
+import random
+from alpaca_trade_api.rest import REST
+
+random.seed(1e3)
+
+last_fetch = {'time': None, 'dict': None}
+api = REST()
+
+# processes the main detailed data for the chart
+def process_pickle():
+    df = pd.read_pickle("db/holdings")
+
+    # update prices for stocks - replace 'Last ($)' and 'Market Value ($)' using 'Quantity'
+    is_stock = df['Product Type'] == 'Stocks / Options'
+    stock = df[is_stock]
+
+    bar = api.get_barset(list(stock['Symbol']), 'day', limit=1)
+    bar_df = pd.DataFrame(data={'Symbol': bar.keys(), 'Last ($)': [o[0].c for o in bar.values()]})
+    stock = stock.drop('Last ($)', axis=1)
+    stock = stock.join(bar_df.set_index('Symbol'), on='Symbol')
+    stock['Market Value ($)'] = stock['Last ($)'] * stock['Quantity']
+
+    non_stock = df[[not e for e in is_stock]]
+    df = pd.concat([stock, non_stock])
+
+    df = df.fillna('')  # js not happy about NaN
+    df.sort_values("Market Value ($)", inplace=True, ascending=False)
+
+    # group by sector with equity objects (convert tuples to dicts)
+    df['dict'] = df[list(df.columns)].to_dict("records")
+    dict_df = df[['Sector', 'dict']]
+    df.drop(labels='dict', axis=1)
+    dict_df = dict_df.groupby('Sector')['dict'].apply(list)
+    detailed = dict_df.to_dict()
+
+    return detailed
+
+
+# produces the outer ring data for the chart
+def by_sector(detailed):
+    sector_sums = defaultdict(lambda: 0)
+    for sector in detailed.keys():
+        for equity in detailed[sector]:
+            sector_sums[sector] += equity["Market Value ($)"]
+    portfolio_sum = sum(sector_sums.values())
+
+    sector_data = []
+    for sector in sector_sums.keys():
+        row = {'sector': sector, 'value': sector_sums[sector], 'pct': float(sector_sums[sector] / portfolio_sum)}
+        sector_data.append(row)
+
+    random.shuffle(sector_data)     # to avoid small sections next to each other
+
+    return sector_data
+
+
+# splits the detailed df into data structured for the chart
+def into_d3_dict(detailed):
+    return {"byEquity": detailed, "bySector": by_sector(detailed)}
+
+
+# avoids fetching more than every 5 minutes
+def portfolio_dictionary():
+    global last_fetch
+    t = last_fetch['time']
+    if (t is None) or ((datetime.datetime.now() - t) > datetime.timedelta(minutes=5)):
+        last_fetch = {'time': datetime.datetime.now(), 'dict': into_d3_dict(process_pickle())}
+    return last_fetch["dict"]
